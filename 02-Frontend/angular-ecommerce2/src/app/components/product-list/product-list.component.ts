feacbcbbdebb56ddf98e3d249092cb83afca1ba1
Lefteris Soulis <lefteris95.soulis@gmail.com>
2021-11-11 06:29:21 -0800

    Search by keyword pagination

--- 02-Frontend/angular-ecommerce2/src/app/components/product-list/product-list.component.ts
+++ 02-Frontend/angular-ecommerce2/src/app/components/product-list/product-list.component.ts
@@ -12,67 +12,67 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number = 1;
-  previousCategoryId: number = 1;
   searchMode: boolean = false;
+  previousCategoryId: number = 1;
 
-  // properties for pagination
+  // new properties for pagination
   thePageNumber: number = 1;
   thePageSize: number = 5;
   theTotalElements: number = 0;
-  
+
+  previousKeyword: string = null;
+
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
-      this.listProducts(); 
+      this.listProducts();
     });
   }
 
   listProducts() {
 
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
-    
     if(this.searchMode) {
       this.handleSearchProducts();
     }
-    else {
+    else{
       this.handleListProducts();
     }
-    
   }
 
   handleSearchProducts() {
 
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
-
+  
     // now search for the products using keyword
     this.productService.searchProducts(theKeyword).subscribe(
       data => {
         this.products = data;
       }
     )
-
+  
   }
 
   handleListProducts() {
+     //chekc if "id" parameter is available
+     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-
-    if (hasCategoryId) {
-      //get the "id" param string. convert string to a number using the "+" symbol
-     this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
-    }
+     if (hasCategoryId) {
+       //get the "id" param string. convert string to a number using the "+" symbol
+       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+     }
      else {
        // not category id available ... default to category id 1
        this.currentCategoryId = 1;
      }
-
-     // check if we have a different category than previous
-     // Note: Angular will reuse a component if it is currently being viewed
+ 
      //
+     // Check if we have a different category than previous
+     // Note: Angular will reuse a component if is currently being viewed
 
-     // if we have a different category id than previous 
+     // if we have a different category id than previous
      // then set thePageNumber back to 1
      if(this.previousCategoryId != this.currentCategoryId) {
        this.thePageNumber = 1;
@@ -82,23 +82,28 @@ export class ProductListComponent implements OnInit {
      console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
 
      // now get the products for the given category id
-    // this.productService.getProductList(this.currentCategoryId).subscribe(
-    //   data => {
-    //     this.products = data;
-    //   }
-    // )
-    this.productService.getProductListPaginate(this.thePageNumber - 1,
-                                               this.thePageSize,
-                                               this.currentCategoryId)
-                                               .subscribe(this.processResult());
+     this.productService.getProductListPaginate(this.thePageNumber-1,
+                                                this.thePageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());
   }
 
   processResult() {
-    return data =>{
+    // map the data
+    return data => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    // to refresh the page view
+    this.listProducts();
+  }
 }
+
+
