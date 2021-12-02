1e7662453486a2957fd6ee76adbe98cd122ed5cc
leFos-95 <lefteris95.soulis@gmail.com>
2021-12-02 17:48:47 +0200

    Update refresh tables

--- 01-starter-files/db-scripts/refresh-database-with-100-products.sql
+++ 01-starter-files/db-scripts/refresh-database-with-100-products.sql
@@ -1,15 +1,15 @@
 -- -----------------------------------------------------
 -- Schema full-stack-ecommerce
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `full-stack-ecommerce`;
+DROP SCHEMA IF EXISTS `full-stack-ecommerce3`;
 
-CREATE SCHEMA `full-stack-ecommerce`;
-USE `full-stack-ecommerce` ;
+CREATE SCHEMA `full-stack-ecommerce3`;
+USE `full-stack-ecommerce3` ;
 
 -- -----------------------------------------------------
 -- Table `full-stack-ecommerce`.`product_category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `full-stack-ecommerce`.`product_category` (
+CREATE TABLE IF NOT EXISTS `full-stack-ecommerce3`.`product_category` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
@@ -19,7 +19,7 @@ AUTO_INCREMENT = 1;
 -- -----------------------------------------------------
 -- Table `full-stack-ecommerce`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `full-stack-ecommerce`.`product` (
+CREATE TABLE IF NOT EXISTS `full-stack-ecommerce3`.`product` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `sku` VARCHAR(255) DEFAULT NULL,
   `name` VARCHAR(255) DEFAULT NULL,
